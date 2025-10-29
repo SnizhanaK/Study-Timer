5ac1d25f79448d9e3a697451b64921ce42fc4f70
Snizhana <snizhanakushynska@gmail.com>
2025-10-29 21:20:26 +0400

    tailwind/lucide

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,92 @@
 <script setup>
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
+import { Cat, Flower, Sun, Moon } from "lucide-vue-next";
+
+const isDark = ref(false);
+
 
 function applyTheme() {
-  if (
+  isDark.value =
       localStorage.theme === "dark" ||
-      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-  ) {
-    document.documentElement.classList.add("dark");
-  } else {
-    document.documentElement.classList.remove("dark");
-  }
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
+  document.documentElement.classList.toggle("dark", isDark.value);
 }
 
 function toggleTheme() {
-  const isDark = document.documentElement.classList.toggle("dark");
-  localStorage.theme = isDark ? "dark" : "light";
+  isDark.value = !isDark.value;
+  document.documentElement.classList.toggle("dark", isDark.value);
+  localStorage.theme = isDark.value ? "dark" : "light";
+}
+
+function cat() {
+  console.log("Cat time 🐱");
+}
+
+function caramel() {
+  console.log("Flower time 🌸");
 }
 
 onMounted(() => applyTheme());
 </script>
 
 <template>
-  <div class="bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen p-8">
-    <button
-        @click="toggleTheme"
-        class="fixed top-4 right-4 bg-gray-200 dark:bg-gray-700 text-sm px-3 py-2 rounded"
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div
+        class="relative w-[390px] h-[780px] bg-white dark:bg-gray-900 text-black dark:text-white rounded-[2rem] shadow-2xl border border-gray-300 overflow-hidden transition-colors"
     >
-      Toggle theme
-    </button>
+      <div class="h-full p-8 relative">
 
-    <h1 class="text-2xl font-bold">Hello world</h1>
-    <p class="text-emerald-900 dark:text-emerald-300 mt-4">Dark mode toggle works!</p>
+        <button
+            @click="toggleTheme"
+            class="absolute top-3 right-3 p-3 rounded-xl  hover:scale-110 active:scale-95 transition cursor-pointer"
+            aria-label="Toggle theme"
+        >
+          <Sun v-if="!isDark" class="w-7 h-7 text-yellow-500" />
+          <Moon v-else class="w-7 h-7 text-gray-100" />
+        </button>
+
+        <button
+            @click="cat"
+            class="absolute top-4 left-4 p-2 rounded-xl  hover:scale-110 active:scale-95 transition cursor-pointer"
+            aria-label="cat time"
+        >
+          <Cat class="w-6 h-6 text-gray-700 dark:text-gray-100" />
+        </button>
+
+        <button
+            @click="caramel"
+            class="absolute top-4 left-15 p-2 rounded-xl  hover:scale-110 active:scale-95 transition cursor-pointer"
+            aria-label="caramel time"
+        >
+          <Flower class="w-6 h-6 text-pink-500 dark:text-pink-300" />
+        </button>
+
+        <div>
+          <div>
+            <button
+                @click="pastDay"
+                class="absolute top-4 left-15 p-2 rounded-xl  hover:scale-110 active:scale-95 transition cursor-pointer"
+                aria-label="caramel time"
+            >
+              <Flower class="w-6 h-6 text-pink-500 dark:text-pink-300" />
+            </button>
+          </div>
+          <h1 class="mt-12 text-1xl font-bold">Hello world</h1>
+          <div>
+            <button
+                @click="pastDay"
+                class="absolute top-4 left-15 p-2 rounded-xl  hover:scale-110 active:scale-95 transition cursor-pointer"
+                aria-label="caramel time"
+            >
+              <Flower class="w-6 h-6 text-pink-500 dark:text-pink-300" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-</style>
 
+</style>
