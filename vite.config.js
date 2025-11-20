8c6b951fa31cfaf5d33997c03a967d5ece6d7a45
Snizhana <snizhanakushynska@gmail.com>
2025-11-20 12:23:29 +0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/study-timer/',
+    base: '/Study-Timer/',
     plugins: [vue(), tailwindcss()],
 })
